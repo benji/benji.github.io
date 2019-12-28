eff92b7497e869f04005b296d1caf49942a376e0
Benjamin Jaton <benjamin.jaton@gmail.com>
2019-12-28 13:57:45 -0800

    first deployment of language-flashcards

---
+++ language-flashcards/precache-manifest.b1d148e349b4578be164b9f6f67bc970.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a7e400f5a6f26d1667fca4d08521651",
+    "url": "/poc/flashcards/index.html/index.html"
+  },
+  {
+    "revision": "f0be127a5e9475854fae",
+    "url": "/poc/flashcards/index.html/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "34d2bf499058021bc3de",
+    "url": "/poc/flashcards/index.html/static/css/main.75c4996b.chunk.css"
+  },
+  {
+    "revision": "f0be127a5e9475854fae",
+    "url": "/poc/flashcards/index.html/static/js/2.b477cb80.chunk.js"
+  },
+  {
+    "revision": "e86c80c5bedded780e88384c4651f3f3",
+    "url": "/poc/flashcards/index.html/static/js/2.b477cb80.chunk.js.LICENSE"
+  },
+  {
+    "revision": "34d2bf499058021bc3de",
+    "url": "/poc/flashcards/index.html/static/js/main.15a483de.chunk.js"
+  },
+  {
+    "revision": "ad19a337e2b0937f0609",
+    "url": "/poc/flashcards/index.html/static/js/runtime-main.26aae9c8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/poc/flashcards/index.html/static/media/logo.5d5d9eef.svg"
+  }
+]);
