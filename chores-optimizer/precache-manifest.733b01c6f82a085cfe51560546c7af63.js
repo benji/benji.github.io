ba4d3a1570ac77bdfc5bc65f77f8ce12ad534e57
Benjamin Jaton <benjaminjaton@asana.com>
2020-03-15 11:26:41 -0700

    Deploying chores-optimizer

---
+++ chores-optimizer/precache-manifest.733b01c6f82a085cfe51560546c7af63.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c56227a612fc8303fc74aa6b4ee43497",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab2940473d8e4ad38435",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "484f221d34dd320d44ac",
+    "url": "/static/css/main.489a1579.chunk.css"
+  },
+  {
+    "revision": "ab2940473d8e4ad38435",
+    "url": "/static/js/2.88aa3350.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/static/js/2.88aa3350.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "484f221d34dd320d44ac",
+    "url": "/static/js/main.c46f02d5.chunk.js"
+  },
+  {
+    "revision": "1b72847f4f6eb47311c3",
+    "url": "/static/js/runtime-main.a16761aa.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
